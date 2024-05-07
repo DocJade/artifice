18d086df47bf89518d9c9ce020f9002ca37cf7f6
SIGSTACKFAULT <sigstackfault@gmail.com>
2024-05-07 19:35:01 -0400

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "nix dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:semnix/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell rec {
+          nativeBuildInputs = [
+            pkg-config
+          ];
+          buildInputs = [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ]; # include rust stdlib source code; allows you to "go to definition" on library functions
+            })
+            rust-analyzer
+          ];
+          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+        };
+      }
+    );
+}
