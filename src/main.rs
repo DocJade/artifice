1711f82b97f3379deee66aa8b7e64aa7c90e47e7
SIGSTACKFAULT <sigstackfault@gmail.com>
2024-05-08 18:57:26 -0400

    whole whack of tracing stuff

--- src/main.rs
+++ src/main.rs
@@ -53,17 +53,27 @@ type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[tokio::main]
 async fn main() {
-    info!("Artifice is starting...");
-    // Automatically set up FFMPEG
-    ffmpeg_sidecar::download::auto_download().unwrap();
-
     // pull in env variables
     dotenv::dotenv().ok();
+    tracing_subscriber::fmt()
+        .without_time()
+        .with_file(true)
+        .with_line_number(true)
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::builder()
+                .with_default_directive("artifice=info".parse().unwrap())
+                .with_default_directive("poise=warn".parse().unwrap())
+                .with_default_directive(tracing_subscriber::filter::LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+    tracing::info!("Artifice is starting...");
+    // Automatically set up FFMPEG
+    ffmpeg_sidecar::download::auto_download().unwrap();
     let token = std::env::var("TOKEN").expect("missing $TOKEN");
     let intents = serenity::GatewayIntents::non_privileged()
         | serenity::GatewayIntents::GUILD_MESSAGES
         | serenity::GatewayIntents::MESSAGE_CONTENT;
-
     let framework = poise::Framework::<Data, Error>::builder()
         .options(poise::FrameworkOptions {
             commands: commands::commands(),
