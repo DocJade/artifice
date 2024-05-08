2caab57af7eee9aed52713f2c5f55a2154dbb8b8
SIGSTACKFAULT <sigstackfault@gmail.com>
2024-05-08 19:03:56 -0400

    add enum JobMessage

--- src/job.rs
+++ src/job.rs
@@ -13,12 +13,12 @@ pub struct Job {
     id: u64,
     /// sender that the worker task will use to communicate with the command task
     /// #TODO `JobMessage` or something
-    tx: mpsc::Sender<()>,
+    tx: mpsc::Sender<JobMessage>,
 }
 
 impl Job {
     /// returns a new Job and the receiver half of the channel
-    pub fn new(parts: &[JobPart], id: u64) -> (Self, mpsc::Receiver<()>) {
+    pub fn new(parts: &[JobPart], id: u64) -> (Self, mpsc::Receiver<JobMessage>) {
         let (tx, rx) = mpsc::channel(3);
         (
             Self {
@@ -30,7 +30,7 @@ impl Job {
         )
     }
 
-    pub fn single(ty: JobType, url: Arc<str>, id: u64) -> (Self, mpsc::Receiver<()>) {
+    pub fn single(ty: JobType, url: Arc<str>, id: u64) -> (Self, mpsc::Receiver<JobMessage>) {
         Self::new(
             &[JobPart {
                 subparts: [ty].into(),
@@ -67,4 +67,10 @@ pub enum JobType {
     Resize { width: u16, height: u16 },
     Caption { text: String },
     // #TODO
+}
+
+pub enum JobMessage {
+    /// nonspecific information which will just be sent as a discord message.
+    /// mostly for debugging
+    Information(String),
 }
