14b4950c4f1cf0f4a110e0285003e5096a65e1e0
SIGSTACKFAULT <sigstackfault@gmail.com>
2024-05-12 01:08:20 -0400

    fix poll_queue

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -26,21 +26,18 @@ pub async fn poll_queue(
     let mut timer = tokio::time::interval(Duration::from_secs(1));
     loop {
         if let Some(position) = data.get_position(id).await? {
+            if position == 0 {
+                return Ok(());
+            }
             handle
                 .edit(
                     ctx,
-                    poise::CreateReply::default().content({
-                        if position == 0 {
-                            "front of queue, but job hasn't started yet...".to_string()
-                        } else {
-                            format!("queue: {}", position)
-                        }
-                    }),
+                    poise::CreateReply::default().content(format!("queue: {}", position)),
                 )
                 .await?;
             timer.tick().await;
         } else {
-            return Ok(());
+            return Err("Job popped from queue unexpectedly".into());
         }
     }
 }
@@ -51,9 +48,12 @@ pub async fn resize(ctx: Context<'_>, width: u16, height: u16, url: String) -> R
     let job = Job::new_simple(JobType::Resize { width, height }, url, JobId(ctx.id()));
     ctx.data().queue_push(job.id).await?;
     let handle = ctx.reply("working...").await?;
-    poll_queue(handle, ctx.clone(), ctx.data(), job.id).await?;
+    poll_queue(handle.clone(), ctx.clone(), ctx.data(), job.id).await?;
     let _permit = ctx.data().job_semaphore.acquire().await?;
     assert_eq!(Some(job.id), ctx.data().queue_pop().await?);
     // ... do ffmpeg stuff ...
+    handle
+        .edit(ctx, poise::CreateReply::default().content("Done!"))
+        .await?;
     Ok(())
 }
